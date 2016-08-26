8f79a9484d7149ae7a21771051899dedeb2dc6ce
Nick Jacoby <nickjacoby22@gmail.com>
2016-08-25 19:31:28 -0700

    trying out app.get

--- scripts/repo.js
+++ scripts/repo.js
@@ -6,17 +6,19 @@
   // TODO: Refactor and simplify this ajax call into a get request
   // to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    proxyGitHub(
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      success: function(data) {
-        repos.all = data;
-      }
-    }).done(callback));
-  };
+    app.get('https://api.github.com/users/brookr/repos' +
+          '?per_page=100' +
+          '&sort=updated', proxyGitHub());
+  //   $.ajax({
+  //     url: 'https://api.github.com/users/brookr/repos' +
+  //           '?per_page=100' +
+  //           '&sort=updated',
+  //     type: 'GET',
+  //     success: function(data) {
+  //       repos.all = data;
+  //     }
+  //   }).done(callback));
+  // };
 
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
